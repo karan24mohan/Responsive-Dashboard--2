5b5d511984367257270e569bbeaf855d68b46c71
karan24mohan <karanmohan2410@gmail.com>
2023-08-01 22:30:50 +0530

    reverse js changes

--- script.js
+++ script.js
@@ -39,12 +39,12 @@ searchBtn.addEventListener("click", function (e) {
 });
 
 window.addEventListener("resize", () => {
-  if (window.innerWidth < "768px") {
+  if (window.innerWidth < 768) {
     sideBar.classList.add("close");
   } else {
     sideBar.classList.remove("close");
   }
-  if (window.innerWidth > "576px") {
+  if (window.innerWidth > 576) {
     searchBtnIcon.classList.replace("bx-x", "bx-search");
     searchForm.classList.remove("show");
   }
